ba19630b218e83d4a9f4c87d93b7b21a857dc54e
agupta14 <agupta14@ebay.com>
2015-11-28 19:05:39 +0530

    node_modules restored

--- db.js
+++ db.js
@@ -2,5 +2,5 @@ module.exports = {
     //'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
     'url' : process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||'mongodb://localhost/test'
-    
+
 }
